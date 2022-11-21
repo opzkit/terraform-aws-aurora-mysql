de0aaf9971cf903bbff28addffd2efdac145d621
Peter Svensson <peter@sparetimecoders.com>
2022-11-21 15:35:59 +0100

    feat: enable Performance Insights
    For database instace types that supports Performance Insights we enable it by default.
    A 7 day retention of data is free of charge and a longer retention can be set by variables

--- vars.tf
+++ vars.tf
@@ -69,3 +69,11 @@ variable "reader_instance_type" {
   description = "Instance type of writers"
   default     = null
 }
+
+variable "performance_insights_retention_period" {
+  default     = 7
+  description = <<EOT
+  Performance insights is enabled by default, not all instance types are supported: https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/USER_PerfInsights.Overview.Engines.html.
+  Performance insights retention period in days, 7 days is free of charge. Read more here: https://aws.amazon.com/rds/performance-insights/pricing
+EOT
+}
